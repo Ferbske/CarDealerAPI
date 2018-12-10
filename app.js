88031dbd4e730d1090d5bc8e839831c199a6a1fb
Unknown <rick.voesenek@hotmail.com>
2018-12-10 14:10:10 +0100

    missed  a ;

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-})
+});
 
 app.use("/car", require("./routes/car"));
 app.use("/employee", require("./routes/employee"));
