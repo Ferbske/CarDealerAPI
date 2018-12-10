2269c608922163616ee973ad49bc4201e81a510c
Unknown <rick.voesenek@hotmail.com>
2018-12-10 14:06:01 +0100

    Added Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.use("/car", require("./routes/car"));
 app.use("/employee", require("./routes/employee"));
 app.use("/customer", require("./routes/customer"));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get("*", (request, result) => {
     result.status(404);
     result.json("Unknown route");
