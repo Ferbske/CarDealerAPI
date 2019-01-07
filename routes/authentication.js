6157914d6a137e4055cedbca57320e7626240284
Unknown <rick.voesenek@hotmail.com>
2019-01-07 11:29:02 +0100

    JWT and authentication added

---
+++ routes/authentication.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const router = express.Router();
+const auth = require('../authentication/authentication');
+const responseMessages = require("../responseMessages");
+const Isemail = require('isemail');
+const User = require('../src/repositories/userRepo');
+
+router.all(new RegExp("^(?!\/login$|\/register$).*"), (request, response, next) => {
+    const token = request.header('X-Access-Token');
+    auth.decodeToken(token, (error, payload) => {
+        if (error) {
+            responseMessages.ErrorCode401(res);
+        } else {
+            request.user = {username: payload.sub};
+            next();
+        }
+    })
+});
+
+router.post("/register", (req, res) => {
+    const body = req.body;
+    if (!CheckObjects.isValidRegistration(body)) {
+        responseMessages.ErrorCode412MissingValues(res);
+        return;
+    }
+
+    // Get the users information to store in the database.
+    const username = body.username;
+    const email = body.email;
+    const password = body.password;
+    User.createUser(username, email, password, res);
+});
+
+router.post("/login", (req, res) => {
+    const body = req.body;
+    if (!CheckObjects.isValidLogin(body)) {
+        responseMessages.ErrorCode412MissingValues(res);
+        return;
+    }
+    // Get the username and password from the request.
+    const username = body.username;
+    const password = body.password;
+    User.login(username, password, res);
+});
+
+router.post("/user/changepassword", (req, res) => {
+    const body = req.body;
+
+    if (!CheckObjects.isValidPasswordChange(body)) {
+        responseMessages.ErrorCode412MissingValues(res);
+        return;
+    }
+    const password = body.password;
+    const newPassword = body.newPassword;
+    User.changePassword(req.user.username, password, newPassword, res);
+});
+
+router.delete("/user", (req, res) => {
+    const body = req.body;
+
+    if (!CheckObjects.isValidDelete(body)) {
+        responseMessages.ErrorCode412MissingValues(res);
+        return;
+    }
+    const password = body.password;
+    User.deleteUser(req.user.username, password, res);
+});
+
+
+class CheckObjects {
+    // Returns true if the given object is a valid login
+    static isValidLogin(object) {
+        const tmp =
+            object && typeof object == "object" &&
+            object.username && typeof object.username == "string" &&
+            object.password && typeof object.password == "string";
+        console.log(`Is login valid: ${tmp == undefined ? false : tmp}`);
+        return tmp == undefined ? false : tmp;
+    }
+
+    // Returns true if the given object is a valid register
+    static isValidRegistration(object) {
+        const tmp =
+            object && typeof object == "object" &&
+            object.username && typeof object.username == "string" && object.username.length >= 2 &&
+            object.email && typeof object.email == "string" && Isemail.validate(object.email) &&
+            object.password && typeof object.password == "string";
+        console.log(`Is registration valid: ${tmp == undefined ? false : tmp}`);
+        return tmp == undefined ? false : tmp;
+    }
+
+    static isValidPasswordChange(object) {
+        const tmp =
+            object && typeof object == "object" &&
+            object.password && typeof object.password == "string" &&
+            object.newPassword && typeof object.newPassword == "string";
+        console.log(`Is password change valid: ${tmp == undefined ? false : tmp}`);
+        return tmp == undefined ? false : tmp;
+    }
+
+    static isValidDelete(object) {
+        const tmp =
+            object && typeof object == "object" &&
+            object.password && typeof object.password == "string";
+        console.log(`Is account delete valid: ${tmp == undefined ? false : tmp}`);
+        return tmp == undefined ? false : tmp;
+    }
+}
+
+module.exports = router;
---
+++ src/repositories/userRepo.js
@@ -0,0 +1,54 @@
+const auth = require('../authentication/authentication');
+const User = require('../src/user');
+const responseMessages = require("../../responseMessages");
+
+class UserRepository {
+    static createUser(username, email, password, res) {
+        const newUser = new User({ username: username, email: email, password: password });
+
+        User.findOne({username})
+            .then((user) => {
+                if (user === null) {
+                    newUser.save()
+                        .then(() => res.status(200).json({token: auth.encodeToken(username)}))
+                        .catch(() => res.status(500).json(responseMessages.ErrorCode401(res)))
+                } else res.status(420).json(responseMessages.ErrorCode409Duplicate(res));
+            })
+            .catch(() => {res.status(500).json(responseMessages.ErrorCode401(res))}
+            );
+    };
+
+    static login(username, password, res) {
+        User.findOne({username})
+            .then((user) => {
+                if(user.password === password) res.status(200).json({token: auth.encodeToken(username)});
+                else res.status(401).json(responseMessages.ErrorCode401(res));
+            })
+            .catch(() => {res.status(401).json(responseMessages.ErrorCode401(res))});
+
+    };
+
+    static changePassword(username, password, newPassword, res) {
+        User.findOne({username})
+            .then((user) => {
+                console.log("User: " + user);
+                if(user.password === password){
+                    user.set({password: newPassword});
+                    user.save()
+                        .then(() => res.status(200).json({message: "your password has been changed."}))
+                        .catch(() => res.status(500).json(responseMessages.ErrorCode401(res)))
+                } else res.status(401).json(responseMessages.ErrorCode401(res));
+            })
+            .catch(() => {res.status(404).json(responseMessages.ErrorCode422(res))});
+    };
+
+    static deleteUser(username, password, res){
+        User.findOneAndDelete({username, password})
+            .then(() => {
+                res.status(200).json({message: "the user has been deleted."});
+            })
+            .catch(() => {res.status(401).json(responseMessages.ErrorCode401(res))});
+    };
+}
+
+module.exports = UserRepository;
