b53848222ba801ac0a27d76f59bcc5e54e075fd4
Unknown <rick.voesenek@hotmail.com>
2019-01-07 13:20:01 +0100

    changed check on values

--- routes/authentication.js
+++ routes/authentication.js
@@ -18,93 +18,45 @@ router.all(new RegExp("^(?!\/login$|\/register$).*"), (request, response, next) 
 });
 
 router.post("/register", (req, res) => {
-    const body = req.body;
-    if (!CheckObjects.isValidRegistration(body)) {
-        responseMessages.ErrorCode412MissingValues(res);
-        return;
-    }
-
-    // Get the users information to store in the database.
     const username = body.username;
     const email = body.email;
     const password = body.password;
+    if (!username && !email && password) {
+        responseMessages.ErrorCode412MissingValues(res);
+        return;
+    }
     User.createUser(username, email, password, res);
 });
 
 router.post("/login", (req, res) => {
-    const body = req.body;
-    if (!CheckObjects.isValidLogin(body)) {
+    const username = body.username;
+    const password = body.password;
+    if (!username && !password) {
         responseMessages.ErrorCode412MissingValues(res);
         return;
     }
-    // Get the username and password from the request.
-    const username = body.username;
-    const password = body.password;
     User.login(username, password, res);
 });
 
 router.post("/user/changepassword", (req, res) => {
     const body = req.body;
-
-    if (!CheckObjects.isValidPasswordChange(body)) {
+    const password = body.password;
+    const newPassword = body.newPassword;
+    if (!password && !newPassword) {
         responseMessages.ErrorCode412MissingValues(res);
         return;
     }
-    const password = body.password;
-    const newPassword = body.newPassword;
     User.changePassword(req.user.username, password, newPassword, res);
 });
 
 router.delete("/user", (req, res) => {
     const body = req.body;
-
-    if (!CheckObjects.isValidDelete(body)) {
+    const password = body.password;
+    if (!password) {
         responseMessages.ErrorCode412MissingValues(res);
         return;
     }
-    const password = body.password;
     User.deleteUser(req.user.username, password, res);
 });
-
-
-class CheckObjects {
-    // Returns true if the given object is a valid login
-    static isValidLogin(object) {
-        const tmp =
-            object && typeof object == "object" &&
-            object.username && typeof object.username == "string" &&
-            object.password && typeof object.password == "string";
-        console.log(`Is login valid: ${tmp == undefined ? false : tmp}`);
-        return tmp == undefined ? false : tmp;
-    }
-
-    // Returns true if the given object is a valid register
-    static isValidRegistration(object) {
-        const tmp =
-            object && typeof object == "object" &&
-            object.username && typeof object.username == "string" && object.username.length >= 2 &&
-            object.email && typeof object.email == "string" && Isemail.validate(object.email) &&
-            object.password && typeof object.password == "string";
-        console.log(`Is registration valid: ${tmp == undefined ? false : tmp}`);
-        return tmp == undefined ? false : tmp;
-    }
-
-    static isValidPasswordChange(object) {
-        const tmp =
-            object && typeof object == "object" &&
-            object.password && typeof object.password == "string" &&
-            object.newPassword && typeof object.newPassword == "string";
-        console.log(`Is password change valid: ${tmp == undefined ? false : tmp}`);
-        return tmp == undefined ? false : tmp;
-    }
-
-    static isValidDelete(object) {
-        const tmp =
-            object && typeof object == "object" &&
-            object.password && typeof object.password == "string";
-        console.log(`Is account delete valid: ${tmp == undefined ? false : tmp}`);
-        return tmp == undefined ? false : tmp;
-    }
-}
 
 module.exports = router;
