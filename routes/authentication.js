25232c32b9bc0a54a6dcb6b72e742d5368a356db
Unknown <rick.voesenek@hotmail.com>
2019-04-16 17:28:31 +0200

    asd

--- routes/authentication.js
+++ routes/authentication.js
@@ -9,17 +9,17 @@ const User = require('../src/repositories/userRepo');
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({extended: true}));
 
-router.all(new RegExp("^(?!\/login$|\/register$).*"), (req, res, next) => {
-    const token = req.header('X-Access-Token');
-    auth.decodeToken(token, (error, payload) => {
-        if (error) {
-            console.log('Authentication error: ' + error.message);
-        } else {
-            request.user = {username: payload.sub};
-            next();
-        }
-    })
-});
+// router.all(new RegExp("^(?!\/login$|\/register$).*"), (req, res, next) => {
+//     const token = req.header('X-Access-Token');
+//     auth.decodeToken(token, (error, payload) => {
+//         if (error) {
+//             console.log('Authentication error: ' + error.message);
+//         } else {
+//             request.user = {username: payload.sub};
+//             next();
+//         }
+//     })
+// });
 
 router.post("/login", (req, res) => {
     const username = req.body.username;
