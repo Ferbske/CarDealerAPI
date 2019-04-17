42d0720283396de2d73397e4110eb0b189f72b04
Unknown <rick.voesenek@hotmail.com>
2019-04-17 10:14:18 +0200

    asd

--- src/repositories/userRepo.js
+++ src/repositories/userRepo.js
@@ -3,37 +3,39 @@ const User = require('../user');
 const responseMessages = require("../../responseMessages");
 
 class UserRepository {
-    static createUser(username, email, password, res) {
+    static async createUser(username, email, password, res) {
 
-        User.findOne({username})
+        await User.findOne({username})
             .then((user) => {
-                console.log("User found: " + user);
                 if (user === null) {
                     const newUser = new User({
                         username: username,
                         email: email,
                         password: password
                     });
-                    console.log(newUser);
-                    return newUser.save()
+                    newUser.save()
                         .then(() => res.status(200).json({
                             response: 'user has been created',
                             token: auth.encodeToken(username)
                         }))
-                    .catch(() => res.status(500).json(console.log("Creating user failed in user repo 1")))
+                        .catch(() => res.status(500).json(console.log("Creating user failed in user repo 1")))
                 } else responseMessages.ErrorCode409Duplicate(res);
             })
-            .catch(() => { console.log("Creating user failed in user repo 2")}
+            .catch(() => {
+                    console.log("Creating user failed in user repo 2")
+                }
             );
     };
 
     static login(username, password, res) {
         User.findOne({username})
             .then((user) => {
-                if(user.password === password) res.status(200).json({token: auth.encodeToken(username)});
+                if (user.password === password) res.status(200).json({token: auth.encodeToken(username)});
                 else responseMessages.ErrorCode401(res);
             })
-            .catch(() => { responseMessages.ErrorCode401(res)});
+            .catch(() => {
+                responseMessages.ErrorCode401(res)
+            });
 
     };
 
@@ -41,22 +43,26 @@ class UserRepository {
         User.findOne({username})
             .then((user) => {
                 console.log("User: " + user);
-                if(user.password === password){
+                if (user.password === password) {
                     user.set({password: newPassword});
                     user.save()
                         .then(() => res.status(200).json({message: "your password has been changed."}))
                         .catch(() => res.status(500).json(responseMessages.ErrorCode401(res)))
                 } else res.status(401).json(responseMessages.ErrorCode401(res));
             })
-            .catch(() => {res.status(404).json(responseMessages.ErrorCode422(res))});
+            .catch(() => {
+                res.status(404).json(responseMessages.ErrorCode422(res))
+            });
     };
 
-    static deleteUser(username, password, res){
+    static deleteUser(username, password, res) {
         User.findOneAndDelete({username, password})
             .then(() => {
                 res.status(200).json({message: "the user has been deleted."});
             })
-            .catch(() => {res.status(401).json(responseMessages.ErrorCode401(res))});
+            .catch(() => {
+                res.status(401).json(responseMessages.ErrorCode401(res))
+            });
     };
 }
 
